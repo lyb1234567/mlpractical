146d284d76a310345bb70d103dda33c7301414ee
Yanbo Liu <yliu431@sheffield.ac.uk>
2022-10-20 12:07:45 +0100

    finish lab 3

---
+++ test.py
@@ -0,0 +1,96 @@
+import sys
+sys.path.append('C:/Users/admin/Desktop/mlpractical')
+import numpy as np
+import matplotlib.pyplot as plt
+import logging
+from mlp.layers import AffineLayer, SoftmaxLayer, SigmoidLayer
+from mlp.errors import CrossEntropyError, CrossEntropySoftmaxError,SumOfSquaredDiffsError
+from mlp.models import SingleLayerModel, MultipleLayerModel
+from mlp.initialisers import UniformInit
+from mlp.learning_rules import GradientDescentLearningRule
+from mlp.data_providers import MNISTDataProvider
+from mlp.optimisers import Optimiser
+plt.style.use('ggplot')
+
+# Seed a random number generator
+seed = 6102016
+rng = np.random.RandomState(seed)
+
+# Set up a logger object to print info about the training run to stdout
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.handlers = [logging.StreamHandler()]
+
+# Create data provider objects for the MNIST data set
+train_data = MNISTDataProvider('train', rng=rng)
+valid_data = MNISTDataProvider('valid', rng=rng)
+input_dim, output_dim = 784, 10
+def train_model_and_plot_stats(
+        model, error, learning_rule, train_data, valid_data, num_epochs, stats_interval):
+    # As well as monitoring the error over training also monitor classification
+    # accuracy i.e. proportion of most-probable predicted classes being equal to targets
+    data_monitors = {'acc': lambda y, t: (y.argmax(-1) == t.argmax(-1)).mean()}
+
+    # Use the created objects to initialise a new Optimiser instance.
+    optimiser = Optimiser(
+        model, error, learning_rule, train_data, valid_data, data_monitors)
+
+    # Run the optimiser for 5 epochs (full passes through the training set)
+    # printing statistics every epoch.
+    stats, keys, _ = optimiser.train(num_epochs=num_epochs, stats_interval=stats_interval)
+
+    # Plot the change in the validation and training set error over training.
+    fig_1 = plt.figure(figsize=(8, 4))
+    ax_1 = fig_1.add_subplot(111)
+    for k in ['error(train)', 'error(valid)']:
+        ax_1.plot(np.arange(1, stats.shape[0]) * stats_interval,
+                  stats[1:, keys[k]], label=k)
+    ax_1.legend(loc=0)
+    ax_1.set_xlabel('Epoch number')
+
+    # Plot the change in the validation and training set accuracy over training.
+    fig_2 = plt.figure(figsize=(8, 4))
+    ax_2 = fig_2.add_subplot(111)
+    for k in ['acc(train)', 'acc(valid)']:
+        ax_2.plot(np.arange(1, stats.shape[0]) * stats_interval,
+                  stats[1:, keys[k]], label=k)
+    ax_2.legend(loc=0)
+    ax_2.set_xlabel('Epoch number')
+    plt.show()
+    return stats, keys, fig_1, ax_1, fig_2, ax_2
+# Set training run hyperparameters
+batch_size = 128  # number of data points in a batch
+init_scale = 0.1  # scale for random parameter initialisation
+learning_rate = 0.1  # learning rate for gradient descent
+num_epochs = 20  # number of training epochs to perform
+stats_interval = 5  # epoch interval between recording and printing stats
+
+# Reset random number generator and data provider states on each run
+# to ensure reproducibility of results
+rng.seed(seed)
+train_data.reset()
+valid_data.reset()
+
+# Alter data-provider batch size
+train_data.batch_size = batch_size
+valid_data.batch_size = batch_size
+
+# Create a parameter initialiser which will sample random uniform values
+# from [-init_scale, init_scale]
+param_init = UniformInit(-init_scale, init_scale, rng=rng)
+
+# Create affine + softmax model
+model = MultipleLayerModel([
+    AffineLayer(input_dim, output_dim, param_init, param_init),
+    AffineLayer(input_dim, output_dim, param_init, param_init),
+    SoftmaxLayer()
+])
+
+# Initialise a cross entropy error object
+error = SumOfSquaredDiffsError()
+# Use a basic gradient descent learning rule
+learning_rule = GradientDescentLearningRule(learning_rate=learning_rate)
+
+
+_ = train_model_and_plot_stats(
+    model, error, learning_rule, train_data, valid_data, num_epochs, stats_interval)
